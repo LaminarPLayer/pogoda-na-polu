147ec781c927e3a28b36fb129b287638513810f5
Piotr Grzegorczyk <45131434+LaminarPLayer@users.noreply.github.com>
2020-09-27 02:16:02 +0200

    Wyczyszczenie kodu pod zabawę z front-endem :)

--- functions/weather.js
+++ functions/weather.js
@@ -5,18 +5,7 @@ exports.handler = async (event, context) => {
     const lon = event.queryStringParameters.lon || "noLon";
     const apiKey = process.env.OWM_API_KEY;
 
-    let weatherData = {coś: 'to ja'};
-
-    // fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-    //     .then(resp => resp.json())
-    //     .then(resp => {
-    //         console.log(resp);
-    //         weatherData = resp;
-    //     })
-    //     .catch((err) => {
-    //         weatherData = {error: `${err} — Couldn\'t fetch the data. :(`}
-    //     })
-
+    let weatherData;
 
     let response = await fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`);
     let data = await response.json();
@@ -27,34 +16,3 @@ exports.handler = async (event, context) => {
         body: JSON.stringify(weatherData)
     }
 }
-
-// exports.handler = async (event, context) => {
-//     try {
-//         //   const subject = event.queryStringParameters.name || "World"
-//         //   return { statusCode: 200, body: JSON.stringify({ msg: `Hello ${subject}` }) }
-
-//         const lat = event.queryStringParameters.lat || "noLat";
-//         const lon = event.queryStringParameters.lon || "noLon";
-//         const apiKey = process.env.OWM_API_KEY;
-
-//         let weatherData = {coś: 'to ja'};
-
-//         fetch(`http://api.openweathermap.org/data/2.5/onecall?lat=${lat}&lon=${lon}&appid=${apiKey}&units=metric`)
-//             .then(resp => resp.json())
-//             .then(resp => {
-//                 console.log(resp);
-//                 weatherData = resp;
-//             })
-//             .catch((err) => {
-//                 weatherData = {error: `${err} — Couldn\'t fetch the data. :(`}
-//             })
-
-//         return{
-//             statusCode: 200,
-//             body: JSON.stringify(weatherData)
-//         }
-//     }
-//     catch (err) {
-//         return { statusCode: 500, body: err.toString() }
-//     }
-// }
--- public/script.js
+++ public/script.js
@@ -3,9 +3,14 @@ const message = document.getElementById("message")
 let lat = 52.23333;
 let lon = 21.01667;
 
+let weather;
+
 document.querySelector("#button").addEventListener("click", () => {
     fetch(`/.netlify/functions/weather?lat=${lat}&lon=${lon}`)
         .then((data) => data.json())
         // .then(({ msg }) => console.log(msg) || (message.innerHTML = msg))
-        .then(meinSchatz => console.log(meinSchatz))
+        .then(jsonData => {
+            console.log(jsonData);
+            weather = jsonData;
+        })
 })
