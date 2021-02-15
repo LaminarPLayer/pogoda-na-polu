9130287e63efaf7d8171c39cd6a16bd370200e92
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2021-02-15 23:58:20 +0100

    2.0.0.12
    może teraz?

--- public/service-worker.js
+++ public/service-worker.js
@@ -30,20 +30,20 @@ self.addEventListener("install", (event) => {
   self.skipWaiting();
 });
 
-self.addEventListener("activate", (event) => {
-  event.waitUntil(
-    (async () => {
-      // Enable navigation preload if it's supported.
-      // See https://developers.google.com/web/updates/2017/02/navigation-preload
-      if ("navigationPreload" in self.registration) {
-        await self.registration.navigationPreload.enable();
-      }
-    })()
-  );
+// self.addEventListener("activate", (event) => {
+//   event.waitUntil(
+//     (async () => {
+//       // Enable navigation preload if it's supported.
+//       // See https://developers.google.com/web/updates/2017/02/navigation-preload
+//       if ("navigationPreload" in self.registration) {
+//         await self.registration.navigationPreload.enable();
+//       }
+//     })()
+//   );
 
-  // Tell the active service worker to take control of the page immediately.
-  self.clients.claim();
-});
+//   // Tell the active service worker to take control of the page immediately.
+//   self.clients.claim();
+// });
 
 // TESTTT
 self.addEventListener('fetch', function(event) {
