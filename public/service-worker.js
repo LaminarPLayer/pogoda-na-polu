7357b34eb9dddaedf233c4d5287c2eb83c123494
Piotr Grzegorczyk <peter.grzegorczyk@gmail.com>
2021-02-16 01:12:43 +0100

    2.0.1.2
    lets try

--- public/service-worker.js
+++ public/service-worker.js
@@ -12,7 +12,13 @@ self.addEventListener("install", (event) => {
       // Setting {cache: 'reload'} in the new request will ensure that the
       // response isn't fulfilled from the HTTP cache; i.e., it will be from
       // the network.
-      await cache.add(OFFLINE_URL);
+      await cache.addAll([
+            OFFLINE_URL,
+            'logo4.svg',
+            'icons/offline-sun.svg',
+            'icons/offline-cloud.svg',
+            'icons/offline-dead-cloud.svg'
+        ]);
     })()
   );
   // Force the waiting service worker to become the active service worker.
@@ -35,39 +41,42 @@ self.addEventListener("activate", (event) => {
 });
 
 self.addEventListener("fetch", (event) => {
-  // We only want to call event.respondWith() if this is a navigation request
-  // for an HTML page.
-  if (event.request.mode === "navigate") {
+    // We only want to call event.respondWith() if this is a navigation request
+    // for an HTML page.
+    if (event.request.mode === "navigate") {
     event.respondWith(
-      (async () => {
+        (async () => {
         try {
-          // First, try to use the navigation preload response if it's supported.
-          const preloadResponse = await event.preloadResponse;
-          if (preloadResponse) {
+            // First, try to use the navigation preload response if it's supported.
+            const preloadResponse = await event.preloadResponse;
+            if (preloadResponse) {
             return preloadResponse;
-          }
+            }
 
-          // Always try the network first.
-          const networkResponse = await fetch(event.request);
-          return networkResponse;
+            // Always try the network first.
+            const networkResponse = await fetch(event.request);
+            return networkResponse;
         } catch (error) {
-          // catch is only triggered if an exception is thrown, which is likely
-          // due to a network error.
-          // If fetch() returns a valid HTTP response with a response code in
-          // the 4xx or 5xx range, the catch() will NOT be called.
-          console.log("Fetch failed; returning offline page instead.", error);
+            // catch is only triggered if an exception is thrown, which is likely
+            // due to a network error.
+            // If fetch() returns a valid HTTP response with a response code in
+            // the 4xx or 5xx range, the catch() will NOT be called.
+            console.log("Fetch failed; returning offline page instead.", error);
 
-          const cache = await caches.open(CACHE_NAME);
-          const cachedResponse = await cache.match(OFFLINE_URL);
-          return cachedResponse;
+            const cache = await caches.open(CACHE_NAME);
+            const cachedResponse = await cache.match(OFFLINE_URL);
+            return cachedResponse;
         }
-      })()
+        })()
     );
-  }
+    }
+    else{
+        console.log('im here')
+    }
 
-  // If our if() condition is false, then this fetch handler won't intercept the
-  // request. If there are any other fetch handlers registered, they will get a
-  // chance to call event.respondWith(). If no fetch handlers call
-  // event.respondWith(), the request will be handled by the browser as if there
-  // were no service worker involvement.
+    // If our if() condition is false, then this fetch handler won't intercept the
+    // request. If there are any other fetch handlers registered, they will get a
+    // chance to call event.respondWith(). If no fetch handlers call
+    // event.respondWith(), the request will be handled by the browser as if there
+    // were no service worker involvement.
 });
